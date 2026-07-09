7af51cb28ffbf876c666754c0c3e13873bcdac6d
kyzuna28 <122950944+kyzuna28@users.noreply.github.com>
2026-07-09 10:36:16 +0700

    Update manager_identity.h

--- kernel/manager/manager_identity.h
+++ kernel/manager/manager_identity.h
@@ -1,73 +1,33 @@
-#ifndef __KSU_H_KSU_MANAGER
-#define __KSU_H_KSU_MANAGER
+#ifndef __MANAGER_LIST_H
+#define __MANAGER_LIST_H
 
-#include <linux/cred.h>
-#include <linux/types.h>
+struct manager_identity {
+    const char *package_name;
+    const char *hash;
+    const uint32_t sig_size;
+};
 
-#define KSU_INVALID_APPID -1
-#define KSU_PER_USER_RANGE 100000
-
-#ifdef CONFIG_KSU_DISABLE_MANAGER
-static inline bool ksu_is_manager_appid_valid()
-{
-    return true;
-}
-
-static inline bool is_manager()
-{
-    return current_uid().val == 0;
-}
-
-static inline bool is_uid_manager(uid_t uid)
-{
-    return uid == 0;
-}
-
-static inline uid_t ksu_get_manager_appid()
-{
-    return 0;
-}
-
-static inline void ksu_set_manager_appid(uid_t appid)
-{
-    (void)appid;
-}
-
-static inline void ksu_invalidate_manager_uid()
-{
-}
-#else
-extern uid_t ksu_manager_appid; // DO NOT DIRECT USE
-
-static inline bool ksu_is_manager_appid_valid()
-{
-	return ksu_manager_appid != KSU_INVALID_APPID;
-}
-
-static inline bool is_manager()
-{
-	return unlikely(ksu_manager_appid == current_uid().val % KSU_PER_USER_RANGE);
-}
-
-static inline bool is_uid_manager(uid_t uid)
-{
-    return unlikely(ksu_manager_appid == uid % KSU_PER_USER_RANGE);
-}
-
-static inline uid_t ksu_get_manager_appid()
-{
-	return ksu_manager_appid;
-}
-
-static inline void ksu_set_manager_appid(uid_t appid)
-{
-	ksu_manager_appid = appid;
-}
-
-static inline void ksu_invalidate_manager_uid()
-{
-	ksu_manager_appid = KSU_INVALID_APPID;
-}
-#endif
+static const struct manager_identity allowed_managers[] = {
+    {
+        .package_name = "com.rifsxd.ksunext", // KernelSU-Next
+        .hash = "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7", 
+        .sig_size = 0x3e6
+    },
+    {
+        .package_name = "vctsrt.cntgtj.uqfwgg", // KernelSU-Next Spoof
+        .hash = "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7", 
+        .sig_size = 0x3e6
+    },
+    {
+        .package_name = "id.i.bi", // Next Manager Spoof
+        .hash = "1ab6077099505a4f5ff851732d5d965a4908af7f60c871f23b4b3a58e80e6cd3",
+        .sig_size = 0x31c
+    },
+    {
+        .package_name = "id.next.manager", // Next Manager
+        .hash = "1ab6077099505a4f5ff851732d5d965a4908af7f60c871f23b4b3a58e80e6cd3",
+        .sig_size = 0x31c
+    }
+};
 
 #endif
