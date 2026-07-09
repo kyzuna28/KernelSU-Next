86ab92e32bd6ae6f8cb9e964c04b08c58720e732
kyzuna28 <122950944+kyzuna28@users.noreply.github.com>
2026-07-09 11:07:19 +0700

    Update manager_list.h

--- kernel/manager/manager_list.h
+++ kernel/manager/manager_list.h
@@ -24,6 +24,26 @@ static const struct manager_identity allowed_managers[] = {
         .sig_size = 0x31c
     },
     {
+        .package_name = "com.rapli.mambosu", // RapliVx/KernelSU
+        .hash = "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8",
+        .sig_size = 0x384
+    },
+    {
+        .package_name = "com.kowx712.supermanager", // KOWX712/KernelSU
+        .hash = "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588",
+        .sig_size = 0x375
+    },
+    {
+        .package_name = "me.weishu.kernelsu", // rsuntk/KernelSU
+        .hash = "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b",
+        .sig_size = 0x396
+    },
+    {
+        .package_name = "com.twj.wksu", // WildKernels/Wild_KSU
+        .hash = "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4",
+        .sig_size = 0x381
+    },
+    {
         .package_name = "id.next.manager", // Next Manager
         .hash = "1ab6077099505a4f5ff851732d5d965a4908af7f60c871f23b4b3a58e80e6cd3",
         .sig_size = 0x31c
